9e3fad3d543b5af403123dfd9bd22673817b98f8
Tom Illingworth <tom.illingworth@razorcreations.com>
2016-01-06 19:41:45 +0000

    Hello world

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>DC Test</title>
+  <meta name="description" content="A technical test for DC">
+  <meta name="author" content="Tom Illingworth">
+</head>
+
+<body>
+	<h1>Hello World</h1>
+</body>
+</html>
